cedc59939c20ac4a3653df53f0f340856ef724cb
Eric de Vries <hro.mail@gmail.com>
2020-12-01 15:45:15 +0100

    Horrible code but it works

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,34 +1,42 @@
 module Day1 (day1) where
 
 import Lib
-
+import Data.List
 
 readInt :: String -> Int
 readInt = read
 
-add :: Int -> Int -> Int
-add a b = a + b
+convert :: String -> [Int]
+convert s = map readInt (lines s)
 
-isgood :: Int -> Int -> (Int, Int, Int)
-isgood a b
-    | a + b == 2020 = (a, b, a * b)
-    | otherwise = (a, b, fromInteger(-1))
+matchesLength :: Int -> [Int] -> Bool
+matchesLength depth xs = length xs == depth
 
-findSum :: [Int] -> Int -> [(Int, Int, Int)]
-findSum xs y = filter (\(a, b, x) -> x > 0) (map(\x -> isgood x y) xs)
+calculate2 :: [Int] -> [[Int]]
+calculate2 xs = [[x,y] | x <- xs, y <- xs]
 
-findSums :: [Int] -> [(Int, Int, Int)]
-findSums xs = concat (map(\x -> findSum xs x) xs)
+calculate3 :: [Int] -> [[Int]]
+calculate3 xs = [[x,y, z] | x <- xs, y <- xs, z <- xs]
 
-day1Math :: [String] -> [(Int, Int, Int)]
-day1Math n = findSums(map readInt n) 
+is2020 :: [Int] -> Bool
+is2020 xs = sum xs == 2020
+
+calculateResult :: [Int] -> Int
+calculateResult xs = product xs
+
+findResults :: [[Int]] -> [([Int], Int)]
+findResults xs = map (\x -> (x, calculateResult x)) (filter is2020 xs)
+
 
 day1 = do 
     contents <- readFile "data/day1.txt"
     putStrLn ("Data: " ++ (show (lines contents)))
-    let result = day1Math(lines contents)
-    putStrLn ("answer: " ++ (show result))
-
+    -- let result1 = day1Math(lines contents)
+    -- putStrLn ("answer: " ++ (show result1))
+    let result2 = findResults (calculate2 (convert contents))
+    putStrLn ("answer 1: " ++ (show result2))
+    let result3 = findResults (calculate3 (convert contents))
+    putStrLn ("answer 2: " ++ (show result3))
 
 -- :: String -> Integer
 -- day1 let list = []
